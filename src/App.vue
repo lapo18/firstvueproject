a396fcc62b26d30020762fcf1483cecd572925a9
Carlo Sebastian La Porta <carloalaple@gmail.com>
2023-07-03 17:35:05 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,31 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <button @click="show = !show">Menu</button>
+  <Transition name="fade">
+    <VueMenu v-show="show"/>
+  </Transition>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+import VueMenu from "./components/VueMenu.vue"
+export default {
+  name: "App",
+  components: { VueMenu },
+  data() {
+    return {
+        show: false,
+      }
+  }
+  }
+</script>
+
+<style>
+.fade-enter-from,
+.fade-leave-to{
+  opacity: 0;
+}
+.fade-enter-active , 
+.fade-leave-active{
+  transition: opacity 0.3s ease;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/VueMenu.vue
@@ -0,0 +1,8 @@
+<template>
+  <ul>
+    <li>Opcion 1</li>
+    <li>OPcion 2</li>
+    <li>opcione 3</li>
+    <li>opcion 4</li>
+  </ul>
+</template>
